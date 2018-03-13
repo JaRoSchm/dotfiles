ab68809883dfc1b3fae60069ee331631f8932dd3
Jan Robert Schmidt <jaro.schmidt@gmail.com>
2018-03-13 17:52:10 +0100

    inital commit

---
+++ Brewfile
@@ -0,0 +1,54 @@
+# Taps
+tap 'caskroom/cask'
+tap 'caskroom/fonts'
+tap 'homebrew/bundle'
+
+# Install ZSH
+brew 'zsh'
+brew 'zsh-completions'
+
+# Install brew formulae
+brew 'git'
+brew 'nano'
+brew 'tmux'
+brew 'mas'
+
+# Apps
+cask 'appcleaner'
+cask 'atom'
+cask 'calibre'
+cask 'coconutbattery'
+cask 'compositor'
+cask 'dropbox'
+cask 'firefox'
+cask 'geogebra'
+cask 'google-chrome'
+cask 'java'
+cask 'latexit'
+cask 'macdropany'
+cask 'macpass'
+cask 'mactex'
+cask 'microsoft-office'
+cask 'opera'
+cask 'the-unarchiver'
+cask 'torbrowser'
+cask 'virtualbox'
+cask 'whatsapp'
+
+# Fonts
+cask 'font-anonymous-pro'
+
+# Install Mac App Store apps
+mas "BwanaDik", id: 876923947
+mas "GarageBand", id: 682658836
+mas "iStat Mini", id: 927292435
+mas "iStudiez Pro", id: 402989379
+mas "Keynote", id: 409183694
+mas "Magic Number 2", id: 737047715
+mas "MoneyMoney", id: 872698314
+mas "Numbers", id: 409203825
+mas "Outbank", id: 1094255754
+mas "Pages", id: 409201541
+mas "Pixelmator", id: 407963104
+mas "Reeder", id: 880001334
+mas "Tweetbot", id: 557168941
---
+++ install.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+echo "Setting up your Mac."
+
+# Get current dir (so run this script from anywhere)
+export DOTFILES_DIR DOTFILES_CACHE DOTFILES_EXTRA_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew recipes
+brew update
+brew upgrade
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle
+
+# Install sync-settings for Atom
+apm install sync-settings
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install Anaconda
+cd ~/Downloads
+curl -O http://repo.continuum.io/archive/Anaconda3-5.1.0-MacOSX-x86_64.sh
+bash ./Anaconda3-5.1.0-MacOSX-x86_64.sh
+conda update --all --yes
+cd ~
+
+# Make ZSH the default shell environment
+chsh -s $(which zsh)
+
+# Bunch of symlinks
+ln -sfv "$DOTFILES_DIR/symlinks/.zshrc" ~
+ln -sfv "$DOTFILES_DIR/symlinks/.condarc" ~
+ln -sfv "$DOTFILES_DIR/symlinks/.gitconfig" ~
+# ln -sfv "$DOTFILES_DIR/symlinks/.mackup.cfg" ~
+
+# install jupyterthemes
+pip install jupyterthemes
+pip install --upgrade jupyterthemes
+jt -t monokai -ofs 11 -f anonymous -T -N
+
+# Set macOS preferences
+# We will run this last because this will reload the shell
+source .macos
