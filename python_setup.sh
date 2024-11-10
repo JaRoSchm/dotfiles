6a7ddb8cbf0fc99391b4e7626e3eac66b3b15f12
Jan Robert Schmidt <jan_robert.schmidt@tu-dresden.de>
2024-11-10 17:57:55 +0100

    fix vitables install

--- python_setup.sh
+++ python_setup.sh
@@ -1,7 +1,7 @@
 brew install uv
 
 uv tool install ruff --python="/opt/homebrew/bin/python3"
-uv tool install vitables --python="/opt/homebrew/bin/python3"
+uv tool install vitables --python="/opt/homebrew/bin/python3" --with pyside6
 uv tool install proselint --python="/opt/homebrew/bin/python3"
 uv tool install pylyzer --python="/opt/homebrew/bin/python3"
 uv tool install python-lsp-server --with pyls-memestra --with pylsp-rope --python="/opt/homebrew/bin/python3"
